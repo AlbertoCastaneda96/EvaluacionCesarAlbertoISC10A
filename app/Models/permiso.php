1e1e5a5a042c43f7962c6f44dd5a5b0964d3a54f
AlbertoCastaneda96 <148104397+AlbertoCastaneda96@users.noreply.github.com>
2025-12-10 20:22:34 -0600

    Agregados permisos, roles, users seeders y controlador de permisos

---
+++ app/Models/permiso.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class permiso extends Model
+{
+    protected $fillable = [
+        'nombre',
+        'description',
+        'grupo',
+    ];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Laravel\Fortify\Features;
@@ -15,5 +16,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
 });
+
+Route::resource('Users', UserController::class)->names('users');
 
 require __DIR__.'/settings.php';
