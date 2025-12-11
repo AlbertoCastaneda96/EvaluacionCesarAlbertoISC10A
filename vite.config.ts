792fe32f36d07c60d77f36880b75a91ce5f2b8f1
AlbertoCastaneda96 <148104397+AlbertoCastaneda96@users.noreply.github.com>
2025-12-11 04:00:22 -0600

    uploade

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
         tailwindcss(),
         wayfinder({
             formVariants: true,
+            generateOnSave: false, 
         }),
     ],
     esbuild: {
